66c3c3b002e910f28e3d9905f3df1b108a391986
tkumar31 <tkumar31@gmail.com>
2018-06-15 15:47:31 +0530

    added task page feature

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,18 +1,26 @@
 package com.qa.pages;
 
+import org.testng.annotations.Test;
+
 public class LoginPage {
+	
+	@Test
 	public void loginPage(){
 		
 		System.out.println("login page");
 		
 		
 	}
-	
+	@Test
 public void loginSet(){
 		
 		System.out.println("login page");
 		
 		
+	}
+	
+	public void logout(){
+		System.out.println("logout");
 	}
 
 

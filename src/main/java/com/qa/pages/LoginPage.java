7520b7537ebd1ec39b4dea827bebb22649a4c0f7
tkumar31 <tkumar31@gmail.com>
2018-06-15 15:31:56 +0530

    initial commit

---
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -0,0 +1,19 @@
+package com.qa.pages;
+
+public class LoginPage {
+	public void loginPage(){
+		
+		System.out.println("login page");
+		
+		
+	}
+	
+public void loginSet(){
+		
+		System.out.println("login page");
+		
+		
+	}
+
+
+}
---
+++ src/main/java/com/qa/util/TestUtil.java
@@ -0,0 +1,7 @@
+package com.qa.util;
+
+public class TestUtil {
+  public void testutil(){
+	  System.out.println("test util");
+  }
+}
